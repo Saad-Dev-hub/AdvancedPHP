39680fe4c7b117e1631fcaafda04baac29837e56
Saad Elkammah <tamer12345sdf@gmail.com>
2022-01-23 16:57:35 +0200

    Edit File

--- Rename/index.php
+++ Rename/index.php
@@ -1,6 +1,5 @@
 <?php
 //rename all files in the current directory to 1.jpg, 2.jpg, etc.
-var_dump(getimagesize('a50.jpg'));die;
 static $i=1;
 $files = scandir(__DIR__);
 foreach ($files as $file) {
