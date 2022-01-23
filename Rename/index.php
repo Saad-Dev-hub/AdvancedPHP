fde0355bd92e1b7b4016536e17dba0b71be87001
Saad Elkammah <tamer12345sdf@gmail.com>
2022-01-24 00:34:17 +0200

    Add natsort function

--- Rename/index.php
+++ Rename/index.php
@@ -2,6 +2,7 @@
 //rename all files in the current directory to 1.jpg, 2.jpg, etc.
 static $i=1;
 $files = scandir(__DIR__);
+natsort($files);//sort the files in natural order (alphabetical) for every loop
 foreach ($files as $file) {
     if (is_file($file)) {
         if($file=='index.php'){
